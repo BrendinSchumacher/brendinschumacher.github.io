4a36fd1792ffb0c0a339b3eb7a8f2e1a42253f48
BrendinSchumacher <brendinschumacher@live.sheridan.edu>
2016-09-18 17:17:48 -0600

    Revert "Revert "Revert "That was a lot of different styles"""
    This reverts commit b020c7537fa8fa8f42d51eff771b80dc4769784b.
    
    # Conflicts:
    #	INET 2000/week 2 assignment/example.html
    #	INET 2000/week 2 assignment/js/example.js

--- INET 2000/week 2 assignment/js/example.js
+++ INET 2000/week 2 assignment/js/example.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var greeting = 'Howdy ';
 
 var name = 'Molly';
@@ -24,3 +25,31 @@ elShipping.textContent = '$' + shipping;
 elGrandTotal.textContent = '$' + grandTotal;
 
 
+=======
+var greeting = 'Howdy ';
+
+var name = 'Molly';
+
+var message = ', please check your order:';
+
+var tiles = sign.length;
+
+var subTotal = tiles * 5;
+
+var shipping = 7;
+
+var grandTotal = subTotal + shipping;
+elSign.textContent = sign;
+elTiles.textContent = tiles;
+
+
+elSubTotal.textContent = '$' + subTotal;
+
+
+elShipping.textContent = '$' + shipping;
+
+
+elGrandTotal.textContent = '$' + grandTotal;
+
+
+>>>>>>> parent of b020c75... Revert "Revert "That was a lot of different styles""
